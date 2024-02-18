54875c92cbf71dd2638c9f2cc36eff3bf2fb79b9
Laz <lazgerasimov@gmail.com>
2024-02-18 19:01:39 +0200

    setup live cursor sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
+import Live from "@/components/Live";
 
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Next 14 Design Tool</h1>
+    <div>
+      <Live />
     </div>
   );
 }
--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -1,11 +1,20 @@
+import CursorSVG from "@/public/assets/CursorSVG";
 
 
 
-import React from 'react'
+interface CursorProps {
+  color: string;
+  x: number;
+  y: number;
+  message: string;
+}
 
-const Cursor = () => {
+const Cursor = ({ color, x, y, message }: CursorProps) => {
   return (
-    <div>Cursor</div>
+    <div className="pointer-events-none absolute top-0 left-0" style={{ transform: `translateX(${x}px) translateY(${y}px)` }}>
+      <CursorSVG color={color} />
+      {/* Add message */}
+    </div>
   )
 }
 
